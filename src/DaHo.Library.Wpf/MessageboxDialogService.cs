0869153a86537c28b33c0b8d01507b9c8100d8ff
David <davidhodel6@gmail.com>
2019-05-31 17:49:01 +0200

    Added MessageboxDialogService and helper method for displaying validation-errors

---
+++ src/DaHo.Library.Wpf/MessageboxDialogService.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace DaHo.Library.Wpf
+{
+    public class MessageboxDialogService : IDialogService
+    {
+        public bool AskQuestion(string message, string header)
+        {
+            var dialogResult = MessageBox.Show(message, header, MessageBoxButton.YesNo, MessageBoxImage.Question);
+            return dialogResult == MessageBoxResult.Yes;
+        }
+
+        public void ShowInfoDialog(string message, string header)
+        {
+            MessageBox.Show(message, header, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        public void ShowErrorDialog(string message, string header)
+        {
+            MessageBox.Show(message, header, MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        public void ShowWarnDialog(string message, string header)
+        {
+            MessageBox.Show(message, header, MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+    }
+}
--- src/DaHo.Library.Wpf/ViewModelBase.cs
+++ src/DaHo.Library.Wpf/ViewModelBase.cs
@@ -1,5 +1,7 @@
-﻿using System.ComponentModel;
+﻿using System.Collections;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace DaHo.Library.Wpf
 {
@@ -36,6 +38,23 @@ namespace DaHo.Library.Wpf
             storage = value;
             OnPropertyChanged(propertyName);
             return true;
+        }
+
+        /// <summary>
+        /// Displays a warn dialog with all the data errors from the validation.
+        /// </summary>
+        /// <param name="errors">The IEnumerable that contains all the error-objects</param>
+        /// <param name="dialogService">A class implementing the <see cref="IDialogService"/> interface. Through this the warn-dialog is showed to the user.</param>
+        /// <param name="dialogTitle">The title of the warn-dialog which is showed to the user. The default value is "Fehler"</param>
+        protected virtual void DisplayDataErrors(IEnumerable errors, IDialogService dialogService, string dialogTitle = "Fehler")
+        {
+            var sb = new StringBuilder();
+            foreach (var error in errors)
+            {
+                sb.AppendLine($"- {error}");
+            }
+
+            dialogService.ShowWarnDialog(sb.ToString(), dialogTitle);
         }
     }
 }
