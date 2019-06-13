173aa9002d5bec5228853c3d5c10e035ba120044
David <davidhodel6@gmail.com>
2019-06-13 21:54:14 +0200

    Moved projects to src/ folder

---
+++ src/DaHo.Library.AspNetCore/Data/Repositories/Abstractions/IGenericInterface.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DaHo.Library.AspNetCore.Data.Repositories.Abstractions
+{
+    public interface IGenericInterface<T>
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+
+        Task<T> GetByIdAsync(int id);
+
+        Task CreateAsync(T entity);
+
+        Task UpdateAsync(T entity);
+
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ src/DaHo.Library.AspNetCore/Data/Repositories/EntityFramework/GenericEntityInterface.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DaHo.Library.AspNetCore.Data.Repositories.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace DaHo.Library.AspNetCore.Data.Repositories.EntityFramework
+{
+    public abstract class GenericEntityInterface<TEntity> : IGenericInterface<TEntity> where TEntity : class
+    {
+        private readonly DbContext _context;
+
+        protected GenericEntityInterface(DbContext context)
+        {
+            _context = context;
+        }
+
+        public virtual async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _context.Set<TEntity>().ToListAsync();
+        }
+
+        public virtual async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await _context.FindAsync<TEntity>(id);
+        }
+
+        public virtual async Task CreateAsync(TEntity entity)
+        {
+            await _context.Set<TEntity>()
+                .AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task UpdateAsync(TEntity entity)
+        {
+            _context.Set<TEntity>()
+                .Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public virtual async Task DeleteAsync(TEntity entity)
+        {
+            _context.Set<TEntity>()
+                .Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
