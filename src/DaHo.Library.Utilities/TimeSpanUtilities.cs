198a2e27810e129effb0952c08f8299c5578ff22
David <davidhodel6@gmail.com>
2019-06-30 20:21:16 +0200

    Added TimeSpanUtilities

---
+++ src/DaHo.Library.Utilities/TimeSpanUtilities.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DaHo.Library.Utilities
+{
+    public static class TimeSpanUtilities
+    {
+        public static IEnumerable<TimeSpan> GetEveryXMinutesForDay(int minutes)
+        {
+            var startTime = TimeSpan.Zero;
+            foreach (var quarterHourOffset in Enumerable.Range(0, 24 * 60 / minutes))
+            {
+                yield return startTime.Add(TimeSpan.FromMinutes(quarterHourOffset * minutes));
+            }
+        }
+
+        public static TimeSpan RoundToNearest(this TimeSpan a, TimeSpan roundTo)
+        {
+            long ticks = (long)(Math.Round(a.Ticks / (double)roundTo.Ticks) * roundTo.Ticks);
+            return new TimeSpan(ticks);
+        }
+
+        public static bool IsFullHour(this TimeSpan a) => a.Minutes == 0 && a.Seconds == 0 && a.Milliseconds == 0;
+    }
+}
